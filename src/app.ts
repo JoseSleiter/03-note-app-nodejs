f65497b90696fa90d46649bdd9372e0a68d7ff2e
jose sleiter <--global>
2023-04-01 01:33:43 -0400

    Feature: Add helmet security and morga log requests

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import express, { Express } from 'express';
+import helmet from "helmet";
+import morgan from "morgan";
 import mongoose from 'mongoose';
 import noteRoute from './routes/v1/note.routes';
 
@@ -10,9 +12,16 @@ const app: Express = express();
 // Middlewares
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("tiny"));
 
 // Routes
 app.use('/api/v1', noteRoute)
+app.use("*", (_req, res, _next) => {
+    res.send({
+        error: "Unknown resource"
+    });
+});
 
 // Start the server
 const PORT = process.env.PORT || 3000;
